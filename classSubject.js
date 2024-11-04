a91bc2c37a665f2630d458d87d709e0c4a642c4d
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-11-04 02:42:25 -0800

    userProfile

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Introduction to Programming";
+let classSchedule = "9:00 AM - 10:30 AM, Monday and Wednesday";
+let classroom = "Room 121";
+let classInstructor = "Dr.Smith";
+let studentName = "Balaqui Love Amor";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
